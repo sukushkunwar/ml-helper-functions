14e41fbfbd18b57ae7cd1078db9c94ab57b9684b
sukushkunwar <sukush.kunwar@hub24.com.au>
2025-11-17 04:19:49 +0000

    test.py works on local PC

--- test.py
+++ test.py
@@ -1,15 +1,48 @@
-from helpers.data_utils import load_hf_dataset
-
-DATASET_NAME = "conll2003"
+from datasets import load_dataset
+from helpers.data_utils import process_and_get_tokenized_dataset, get_data_collator
+from transformers import AutoTokenizer
+#Configurations
+DATASET_NAME = "lhoestq/conll2003"
 TRAIN_SPLIT = "train"
 TEST_SPLIT = "test"
-VALIDATION_SPLIT = "validation"
-RANDOM_SEED = 42
-MODEL_NAME = ["bert-base-uncased", "finBERT"] #list of models
+MODEL_NAME = ["bert-base-uncased"]
+# load the datasets
+train_ds = load_dataset(DATASET_NAME, split=TRAIN_SPLIT)
+test_ds = load_dataset(DATASET_NAME, split=TEST_SPLIT)
+#load tokenizer
+tokenizer  = AutoTokenizer.from_pretrained(MODEL_NAME[0])
+#parameters
+max_length = 128
+padding = "max_length"
+truncation = True
+#tokenize the dataset
+processed_train_ds = process_and_get_tokenized_dataset(dataset=train_ds,
+                                             tokenizer=tokenizer,
+                                             max_length=max_length,
+                                             padding=padding,
+                                             truncation=truncation)
+processed_test_ds = process_and_get_tokenized_dataset(dataset=test_ds,
+                                              tokenizer=tokenizer,
+                                              max_length=max_length,
+                                             padding=padding,
+                                             truncation=truncation)
+                                             #get data collator
+data_collator = get_data_collator(tokenizer)
+from torch.utils.data import DataLoader
 
 
-#load datasets
-train_ds = load_hf_dataset(DATASET_NAME, TRAIN_SPLIT )
-breakpoint()
-test_ds = load_hf_dataset(DATASET_NAME, TEST_SPLIT )
-val_ds = load_hf_dataset(DATASET_NAME, VALIDATION_SPLIT )
+train_loader = DataLoader(
+    processed_train_ds,
+    batch_size=8,
+    shuffle=True,
+    collate_fn=data_collator
+)
+
+train_loader = DataLoader(
+    processed_test_ds,
+    batch_size=8,
+    collate_fn=data_collator
+)
+
+batch = next(iter(train_loader))
+print(batch)
